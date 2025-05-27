5dbe310281cff9b9f920381351a5eb63ae611c82
tungnk <tungnk1404@gmail.com>
2025-05-27 14:36:30 +0700

    test

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.send({ code: 200, status: true, message: "Successfully", data: [] });
+  res.send({
+    code: 200,
+    status: true,
+    message: "Successfully",
+    data: ["messiu", "cr10"],
+  });
 });
 
 module.exports = router;
