28f07a9c1e223d06b37f1745542c8598a2bf9098
tungnk <tungnk1404@gmail.com>
2025-05-27 11:44:11 +0700

    first commit

---
+++ routes/users.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  res.send({ code: 200, status: true, message: "Successfully", data: [] });
+});
+
+module.exports = router;
